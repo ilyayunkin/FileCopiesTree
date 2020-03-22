273df8a2e7a373947dba9265b094ec964a3391ef
ilyayunkin <ilya.yunkin@ya.ru>
2020-03-23 03:45:26 +0600

    Output to debug versions of C++, Qt, MiGW

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    qDebug() << "C++ version: " << __cplusplus;
+    qDebug() << "Qt version: " << QT_VERSION_STR;
+
+#if __GNUC__
+    qDebug() << "GCC version: " << __MINGW_GCC_VERSION;
+#endif
 
     return a.exec();
 }
